2d06b823ee4901eaa2f01a1c5133e9876b95105c
Brandon Hoskins <branjames117@gmail.com>
2021-12-08 22:13:24 -0600

    prototyping HTML page generator with tailwind css

---
+++ src/generateHTML.js
@@ -0,0 +1,154 @@
+function generateEmployeeHTML(employee) {
+  const name = employee.getName();
+  const role = employee.getRole();
+  const email = employee.getEmail();
+  const id = employee.getID();
+
+  return `
+<div class="flex flex-col shadow-lg rounded-b-2xl mx-5">
+      <h2
+        class="
+          w-100
+          text-xl text-center
+          bg-purple-400
+          py-2
+          rounded-t-2xl
+          border-solid border-2 border-purple-800
+        "
+      >
+        ${name}
+      </h2>
+      <div
+        class="
+          text-md text-left
+          bg-white
+          py-5
+          px-10
+          md:px-5
+          rounded-b-2xl
+          border-solid border-2 border-t-0 border-purple-800
+        "
+      >
+        <div class="table">
+          <div class="table-row">
+            <div class="table-cell pr-5">
+              <i class="${getRoleIcon(role)}"></i>
+            </div>
+            <div class="table-cell">${role}</div>
+          </div>
+          <div class="table-row">
+            <div class="table-cell"><i class="fas fa-id-card"></i></div>
+            <div class="table-cell">${id}</div>
+          </div>
+          <div class="table-row">
+            <div class="table-cell"><i class="far fa-envelope"></i></div>
+            <div class="table-cell">
+              <a href="mailto:${email}"
+                >${email}</a
+              >
+            </div>
+          </div>
+          <div class="table-row">
+            <div class="table-cell"><i class="${getExtraIcon(role)}"></i></div>
+            <div class="table-cell">${getExtraData(employee, role)}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+function getRoleIcon(role) {
+  switch (role) {
+    case 'Manager':
+      return `fas fa-briefcase`;
+    case 'Engineer':
+      return `fas fa-glasses`;
+    case 'Intern':
+      return `fas fa-pencil-alt`;
+  }
+}
+
+function getExtraIcon(role) {
+  switch (role) {
+    case 'Manager':
+      return `far fa-building`;
+    case 'Engineer':
+      return `fab fa-github-square`;
+    case 'Intern':
+      return `fas fa-school`;
+  }
+}
+
+function getExtraData(employee) {
+  switch (role) {
+    case 'Manager':
+      return `${employee.getOfficeNumber()}`;
+    case 'Engineer':
+      return `${employee.getGitHub()}`;
+    case 'Intern':
+      return `${employee.getSchool()}`;
+  }
+}
+
+function generateHTML(team) {
+  return `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Meet Our Team</title>
+    <link
+      href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+      rel="stylesheet"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Readex+Pro&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+      integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <style>
+      body {
+        font-family: 'Readex Pro', sans-serif;
+      }
+    </style>
+  </head>
+  <body class="bg-purple-100 overflow-x-hidden">
+    <!-- Header content here  -->
+    <header
+      class="flex justify-center items-center bg-purple-400 black w-100 h-14"
+    >
+      <h2 class="text-2xl">Meet the Development Team</h2>
+    </header>
+
+    <!-- Main content here -->
+    <main
+      class="
+        container
+        flex flex-row flex-wrap
+        gap-y-8
+        pt-20
+        place-content-center
+        mx-auto
+      "
+    >
+
+    ${team.employee.forEach((employee) => generateEmployeeHTML(employee))}
+
+    </main>
+  </body>
+</html>
+  `;
+}
+
+module.exports = generateHTML;
