ffcf3de5f29d69c967bdc9856a2b4a41683ebf31
Brandon Hoskins <branjames117@gmail.com>
2021-12-08 22:30:48 -0600

    refined generateHTML function with dummy team data

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
+const generateHTML = require('./src/generateHTML');
+
+// dummy data
+const team = [
+  new Manager('Bob Ross', 'bross@gmail.com', 202, '16B'),
+  new Engineer('John Doe', 'jdoe@gmail.com', 333, 'jdoe'),
+  new Engineer('Jane Doe', 'jadoe@gmail.com', 334, 'jadoe'),
+  new Engineer('Collar White', 'cwhite@gmail.com', 337, 'ccwhite'),
+  new Intern('Miike Bolio', 'mbolio@gmail.com', 336, 'Miskatonic Uni'),
+];
+
+console.log(generateHTML(team));
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,61 +1,46 @@
-function generateEmployeeHTML(employee) {
-  const name = employee.getName();
-  const role = employee.getRole();
-  const email = employee.getEmail();
-  const id = employee.getID();
+function generateEmployeeHTML(team) {
+  let html = ``;
 
-  return `
-<div class="flex flex-col shadow-lg rounded-b-2xl mx-5">
-      <h2
-        class="
-          w-100
-          text-xl text-center
-          bg-purple-400
-          py-2
-          rounded-t-2xl
-          border-solid border-2 border-purple-800
-        "
-      >
-        ${name}
-      </h2>
-      <div
-        class="
-          text-md text-left
-          bg-white
-          py-5
-          px-10
-          md:px-5
-          rounded-b-2xl
-          border-solid border-2 border-t-0 border-purple-800
-        "
-      >
-        <div class="table">
-          <div class="table-row">
-            <div class="table-cell pr-5">
-              <i class="${getRoleIcon(role)}"></i>
+  team.forEach((employee) => {
+    const name = employee.getName();
+    const role = employee.getRole();
+    const roleIcon = getRoleIcon(role);
+    const email = employee.getEmail();
+    const id = employee.getID();
+    const extraIcon = getExtraIcon(role);
+    const extraData = getExtraData(employee, role);
+
+    html += `
+      <div class="flex flex-col shadow-lg rounded-b-2xl mx-5">
+        <h2 class="w-100 text-xl text-center bg-purple-400 py-2 rounded-t-2xl border-solid border-2 border-purple-800">${name}</h2>
+        <div class="text-md text-left bg-white py-5 px-10 md:px-5 rounded-b-2xl border-solid border-2 border-t-0 border-purple-800">
+          <div class="table">
+            <div class="table-row">
+              <div class="table-cell pr-5">
+                <i class="${roleIcon}"></i>
+              </div>
+              <div class="table-cell">${role}</div>
             </div>
-            <div class="table-cell">${role}</div>
-          </div>
-          <div class="table-row">
-            <div class="table-cell"><i class="fas fa-id-card"></i></div>
-            <div class="table-cell">${id}</div>
-          </div>
-          <div class="table-row">
-            <div class="table-cell"><i class="far fa-envelope"></i></div>
-            <div class="table-cell">
-              <a href="mailto:${email}"
-                >${email}</a
-              >
+            <div class="table-row">
+              <div class="table-cell"><i class="fas fa-id-card"></i></div>
+              <div class="table-cell">${id}</div>
             </div>
-          </div>
-          <div class="table-row">
-            <div class="table-cell"><i class="${getExtraIcon(role)}"></i></div>
-            <div class="table-cell">${getExtraData(employee, role)}</div>
+            <div class="table-row">
+              <div class="table-cell"><i class="far fa-envelope"></i></div>
+              <div class="table-cell">
+                <a href="mailto:${email}">${email}</a>
+              </div>
+            </div>
+            <div class="table-row">
+              <div class="table-cell"><i class="${extraIcon}"></i></div>
+              <div class="table-cell">${extraData}</div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  `;
+      </div>`;
+  });
+
+  return html;
 }
 
 function getRoleIcon(role) {
@@ -80,12 +65,13 @@ function getExtraIcon(role) {
   }
 }
 
-function getExtraData(employee) {
+function getExtraData(employee, role) {
   switch (role) {
     case 'Manager':
       return `${employee.getOfficeNumber()}`;
     case 'Engineer':
-      return `${employee.getGitHub()}`;
+      const github = employee.getGitHub();
+      return `<a href="${github}" target="_new">${github}</a>`;
     case 'Intern':
       return `${employee.getSchool()}`;
   }
@@ -100,16 +86,10 @@ function generateHTML(team) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Meet Our Team</title>
-    <link
-      href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
-      rel="stylesheet"
-    />
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Readex+Pro&display=swap"
-      rel="stylesheet"
-    />
+    <link href="https://fonts.googleapis.com/css2?family=Readex+Pro&display=swap" rel="stylesheet" />
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
@@ -124,26 +104,12 @@ function generateHTML(team) {
     </style>
   </head>
   <body class="bg-purple-100 overflow-x-hidden">
-    <!-- Header content here  -->
-    <header
-      class="flex justify-center items-center bg-purple-400 black w-100 h-14"
-    >
+    <header class="flex justify-center items-center bg-purple-400 black w-100 h-14">
       <h2 class="text-2xl">Meet the Development Team</h2>
     </header>
 
-    <!-- Main content here -->
-    <main
-      class="
-        container
-        flex flex-row flex-wrap
-        gap-y-8
-        pt-20
-        place-content-center
-        mx-auto
-      "
-    >
-
-    ${team.employee.forEach((employee) => generateEmployeeHTML(employee))}
+    <main class="container flex flex-row flex-wrap gap-y-8 pt-20 place-content-center mx-auto">
+    ${generateEmployeeHTML(team)}
 
     </main>
   </body>
